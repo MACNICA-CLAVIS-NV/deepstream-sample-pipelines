87c6e1d590128192240e48955d3c19608f6ac495
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2021-06-02 10:54:59 +0900

    Update usbcam_infer_jetson.sh

--- usbcam_infer_jetson.sh
+++ usbcam_infer_jetson.sh
@@ -3,7 +3,7 @@
 #
 # MIT License
 # 
-# Copyright (c) 2019, 2020, 2021 MACNICA Inc.
+# Copyright (c) 2019 MACNICA Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
